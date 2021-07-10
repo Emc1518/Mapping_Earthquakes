2eea3ccf9eef99b656a5307d5fff976debbadf9f
Emc1518 <81889167+Emc1518@users.noreply.github.com>
2021-07-10 18:55:58 -0400

    tdot

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,56 +1,39 @@
 
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+
 
   //We create the tile layer that will be the background of our map.
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 //dark map
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 }); 
 
-//light map
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-}); 
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets,
+};
 
-
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//createa the map object with center, zoom level and default layer
+let map = L.map('mapid', {
+  center: [43.7, -79.3], 
+  zoom: 11, 
+  layers: [satelliteStreets]
+});
 
 // Accessing the Toronto airline routes GeoJSON URL 
-let torontoData = "https://raw.githubusercontent.com/Emc1518/Mapping_Earthquakes/main/torontoRoutes.json";
-
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
+let torontoHoods = "https://raw.githubusercontent.com/Emc1518/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-  style: myStyle,
-  oneachFeature: function(feature, layer){
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination:" 
-    + feature.properties.dst + "</h3>");
-  }
-})
-
-
-.addTo(map);
+    L.geoJSON(data).addTo(map)
 });
 
 
